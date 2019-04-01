0cf48a49c4d41c26e4d4177c10cca9c755a08944
blazarow09 <blazarow09@gmail.com>
2019-04-01 20:59:02 +0300

    upload accordition script

---
+++ scripts/main/AccordionInit.js
@@ -0,0 +1,13 @@
+$( function accordionInit() {
+    let icons = {
+        header: "ui-icon-star",
+        activeHeader: "ui-icon-star"
+    };
+
+    $( "#accordion" ).accordion({
+        heightStyle: "content",
+        collapsible: true,
+        icons : icons,
+        border: "1px solid black"
+    });
+} );
