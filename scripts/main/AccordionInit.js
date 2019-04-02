04d22c0568766498d62ae0a7665caf7124103d6f
Borislav <40491511+blazarow09@users.noreply.github.com>
2019-04-02 11:49:41 +0300

    changed selector element

--- scripts/main/AccordionInit.js
+++ scripts/main/AccordionInit.js
@@ -1,13 +1,13 @@
-$( function accordionInit() {
+(function accordionInit() {
     let icons = {
         header: "ui-icon-star",
         activeHeader: "ui-icon-star"
     };
 
-    $( "#accordion" ).accordion({
+    $( ".accordion" ).accordion({
         heightStyle: "content",
         collapsible: true,
         icons : icons,
         border: "1px solid black"
-    });
+    }); 
 } );
