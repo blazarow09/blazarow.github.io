820ec21c95683ac94a0357f182a6d741a9cb6d95
blazarow09 <blazarow09@gmail.com>
2019-04-19 14:16:33 +0300

    updated script

--- scripts/main/ProgressBarIncrease.js
+++ scripts/main/ProgressBarIncrease.js
@@ -8,33 +8,31 @@
     });
 })();
 
-function InitializeBars(direction) {
+function InitializeBars() {
     $(function() {
-
-      
             $("#pb1").animate({
-                width: "60%"
+                width: "70%"
             }, 999);
             $("#pb2").animate({
-                width: "40%"
+                width: "55%"
             }, 999);
             $("#pb3").animate({
-                width: "50%"
+                width: "60%"
             }, 999);
             $("#pb4").animate({
                 width: "70%"
             }, 999);
             $("#pb5").animate({
-                width: "60%"
+                width: "70%"
             }, 999);
             $("#pb6").animate({
-                width: "70%"
+                width: "75%"
             }, 999);
             $("#pb7").animate({
-                width: "70%"
+                width: "75%"
             }, 999);
             $("#pb8").animate({
-                width: "60%"
+                width: "65%"
             }, 999);
             $("#pb9").animate({
                 width: "90%"
