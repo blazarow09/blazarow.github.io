4d01ffd6ced47e46789ebea11623ec99c6a764cf
blazarow09 <blazarow09@gmail.com>
2019-04-25 15:53:26 +0300

    progress bar script change

--- scripts/main/ProgressBarIncrease.js
+++ scripts/main/ProgressBarIncrease.js
@@ -2,28 +2,26 @@
     new Waypoint.Inview({
         element: $('.progress'),
         entered: function (direction) {
-            InitializeBars(direction);
+            InitializeBars();
             this.destroy();
         },
     });
 })();
 
-function InitializeBars(direction) {
-    if(direction === 'down'){
-        $(function() {
-            let bars = $('#progressBars .bar');
+function InitializeBars() {
+    
+	$(function() {
+		let bars = $('#progressBars .bar');
 
-            for (let i = 0; i < bars.length; i++) {
-                const bar = bars[i];
-                let percent = $(bar).attr('aria-valuemax');
+		for (let i = 0; i < bars.length; i++) {
+			const bar = bars[i];
+			let percent = $(bar).attr('aria-valuemax');
 
-                $(bar).animate({
-                    width: `${percent}%`
-                }, 999)
-            }
-        });
-    }
-
+			$(bar).animate({
+				width: `${percent}%`
+			}, 999)
+		}
+	});
 }
 
 
